725fdbee30c638e9c773cfd7c07beca4f4b57877
Joshua Burnett <joshuaburnett429@gmail.com>
2023-06-19 23:20:05 -0500

    router.delete functioning

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,12 +5,8 @@ const {
   validatePost,
 } = require('../middleware/middleware')
 
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
-
 const User = require('./users-model')
 const Post = require('../posts/posts-model')
-
 const router = express.Router();
 
 router.get('/',  (req, res, next) => {
@@ -33,15 +29,24 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  console.log(req.user);
-  console.log(req.name)
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user);
+router.delete('/:id', validateUserId, async (req, res) => {
+  try {
+    await User.remove(req.params.id)
+    res.json(req.user)
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
